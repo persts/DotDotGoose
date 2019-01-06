f4b0cbf27ef2c6869ab11f34a93eb99fcf813257
pete <ersts@amnh.org>
2019-01-06 16:01:12 -0500

    Updated comment blocks

--- ddg/__init__.py
+++ ddg/__init__.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# DotDotGoose
+# Author: Peter Ersts (ersts@amnh.org)
+#
+# --------------------------------------------------------------------------
+#
+# This file is part of the DotDotGoose application.
+# DotDotGoose was forked from the Neural Network Image Classifier (Nenetic).
+#
+# DotDotGoose is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DotDotGoose is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with with this software.  If not, see <http://www.gnu.org/licenses/>.
+#
+# --------------------------------------------------------------------------
 from .canvas import Canvas  # noqa: F401
 from .point_widget import PointWidget  # noqa: F401
 from .central_widget import CentralWidget  # noqa: F401
--- ddg/fields.py
+++ ddg/fields.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 #
-# Dot-Dot-Goose
-# Copyright (C) 2018 Peter Ersts
-# ersts@amnh.org
+# DotDotGoose
+# Author: Peter Ersts (ersts@amnh.org)
 #
 # --------------------------------------------------------------------------
 #
-# This file is part of the Dot-Dot-Goose application.
-# Dot-Dot-Goose was forked from the Neural Network Image Classifier (Nenetic).
+# This file is part of the DotDotGoose application.
+# DotDotGoose was forked from the Neural Network Image Classifier (Nenetic).
 #
-# Dot-Dot-Goose is free software: you can redistribute it and/or modify
+# DotDotGoose is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Dot-Dot-Goose is distributed in the hope that it will be useful,
+# DotDotGoose is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
